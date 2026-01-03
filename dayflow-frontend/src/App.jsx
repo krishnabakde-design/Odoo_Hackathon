9bda830ac70540e0ef646b01f2d8ac4cc3cc3f96
Ayush Sharma <Ayush.sharma@avantika.edu.in>
2026-01-03 11:14:59 +0530

    Tailwind , node and all other working modules live

---
+++ dayflow-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-500">
+      <h1 className="text-6xl font-black text-white">
+        TAILWIND FINALLY WORKING
+      </h1>
+    </div>
+  );
+}
